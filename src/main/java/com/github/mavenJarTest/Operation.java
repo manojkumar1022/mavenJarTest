b662a52c0d2f1d8acbc7459a586170aeb3315570
Manoj Kumar <61936130+manojkumar1022@users.noreply.github.com>
2022-03-28 16:50:56 +0530

    first commit

---
+++ src/main/java/com/github/mavenJarTest/Operation.java
@@ -0,0 +1,10 @@
+package com.github.mavenJarTest;
+
+import java.math.BigDecimal;
+
+public class Operation {
+    public static BigDecimal add(BigDecimal a, BigDecimal b){return a.add(b);}
+    public static BigDecimal subtract(BigDecimal a, BigDecimal b){return a.subtract(b);}
+    public static BigDecimal mulitply(BigDecimal a, BigDecimal b){return a.multiply(b);}
+    public static BigDecimal divide(BigDecimal a, BigDecimal b){return a.divide(b);}
+}
